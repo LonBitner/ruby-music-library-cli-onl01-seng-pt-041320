9e2c72f7e0e5945741aadb63f87953cccf570ea0
Lon Bitner <lonbitner@gmail.com>
2020-05-27 21:33:52 +0000

    Automatically backed up by Learn

--- lib/Concerns::Findable.rb
+++ lib/Concerns::Findable.rb
@@ -3,5 +3,6 @@ module Concerns::Findable
     self.all.detect {|e| e.name == name}
   end
   
-  def f
-end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
