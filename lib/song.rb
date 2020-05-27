9c263c5d4b400df2ea7f527b7219fc7c4da96391
Lon Bitner <lonbitner@gmail.com>
2020-05-27 21:26:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,12 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end
   
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name == name
+    end
+  end
+  
   def self.all
     @@all
   end
