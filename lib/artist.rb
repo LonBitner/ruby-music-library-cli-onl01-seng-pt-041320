7f1ec0d9dc49bf2828d54f16ce45389e8b81399b
Lon Bitner <lonbitner@gmail.com>
2020-05-27 21:45:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,49 @@
 class Artist
-  
-  attr_accessor :name, :songs, :musicimporter, :musiclibrarycontroller
+  attr_accessor :name, :song, :musiclibrarycontroller, :musicimporter
   extend Concerns::Findable
-  
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
-    save
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
-    @@all = []
+    @@all.clear
   end
-  
+
   def save
     @@all << self
   end
-  
+
   def self.create(artist)
-    self.new(artist)
+    artist = self.new(artist)
+    artist.save
+    artist
   end
-  
+
+  def songs
+    @songs
+  end
+
   def add_song(song)
     if song.artist == nil
       song.artist = self
+    else
+      nil
     end
-    
-    if !@songs.include?(song)
+    if @songs.include?(song)
+      nil
+    else
       @songs << song
     end
+    song
   end
-  
+
   def genres
     @new_array = []
     @songs.each do |song|
@@ -48,4 +55,5 @@ class Artist
     end
     @new_array
   end
+
 end
