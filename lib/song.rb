3735be305f322594e7ff68aa8d4e706fea1484f9
Lon Bitner <lonbitner@gmail.com>
2020-05-27 21:45:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,49 +1,69 @@
 class Song
-  
-  attr_accessor :name, :genre, :musicimporter, :musiclibrarycontroller
+  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
   extend Concerns::Findable
-  
+
   @@all = []
-  
-  def initialize(name, artist = nil, genre = nil)
+
+  def initialize(name, artist=nil, genre=nil)
     @name = name
-    if artist != nil
-      self.artist = artist
-    end
-    if genre != nil
-      self.genre = genre
-    end
-    save
+    self.artist=(artist) if artist != nil
+    self.genre=(genre) if genre != nil
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(song)
+    song = self.new(song)
+    song.save
+    song
+  end
+
   def artist
     @artist
   end
-  
+
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
   end
-  
-   def genre
+
+  def genre
     @genre
   end
-  
+
   def genre=(genre)
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
   end
-  
+
   def self.find_by_name(name)
     @@all.detect do |song|
       song.name == name
     end
   end
-  
+
   def self.find_or_create_by_name(name)
+    # @@all.detect do |song|
+    #   if song.name == name
+    #     song
+    #   else
+    #     self.create(name)
+    #   end
+    # end
     self.find_by_name(name) || self.create(name)
+
   end
-  
+
   def self.new_from_filename(filename)
     array = filename.split(" - ")
 
@@ -59,21 +79,6 @@ class Song
   def self.create_from_filename(filename)
     self.new_from_filename(filename).save
   end
-  
-  def self.all
-    @@all
-  end
-  
-  def self.destroy_all
-    @@all = []
-  end
-  
-  def save
-    @@all << self
-  end
-  
-  def self.create(song)
-    self.new(song)
-  end
+
 
 end
